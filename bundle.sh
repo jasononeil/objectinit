0cbf80e8a91fe813d237140c6996d6f98c4b2463
Jason O'Neil <jason.oneil@gmail.com>
2015-03-10 16:20:27 +0800

    Include demo in zip file

--- bundle.sh
+++ bundle.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+haxe demo.hxml || exit
+
 libname='objectinit'
 rm -f "${libname}.zip"
-zip -r "${libname}.zip" haxelib.json src README.md
+zip -r "${libname}.zip" haxelib.json src demo README.md
 echo "Saved as ${libname}.zip"
